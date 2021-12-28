5c014e904967ac11d9661fa6ea6e2d155906a31b
Kevin Scharfenberg <kelu3227@gmail.com>
2021-12-28 22:35:56 +0100

    bugfix Data source

--- source/API/Entities/Product.cs
+++ source/API/Entities/Product.cs
@@ -8,7 +8,7 @@ namespace API.Entities
         public long Price { get; set; }
         public string PictureUrl { get; set; }
         public string Type {get;set;}
-        public string Brund { get; set; }
+        public string Brand { get; set; }
         public int QuantityInStock { get; set; }
     }
 }
